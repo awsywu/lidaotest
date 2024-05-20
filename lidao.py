7ffa49627fed51b23ed5d22f056e28497a5fd6ff
Yapeng Wu <yapengwu@gmail.com>
2024-05-20 09:53:56 -0400

    updated qqq diagram

--- lidao.py
+++ lidao.py
@@ -1,16 +1,26 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-# Fetch the data
-qqq = yf.Ticker("QQQ")
-data = qqq.history(period="1mo")  # You can change the period as needed
+# Fetching the QQQ data starting from a specific date
+start_date = '2024-05-01'
+qqq_data = yf.download('QQQ', start=start_date)
 
-# Plot the closing prices
+# Filtering the 'Close' prices
+closing_prices = qqq_data['Close']
+
+# Setting up the plot
 plt.figure(figsize=(10, 5))
-plt.plot(data.index, data['Close'], label='QQQ Closing Price')
-plt.title('Daily Closing Prices of QQQ')
+plt.plot(closing_prices.index, closing_prices, linestyle='-', marker='o', color='b', markersize=5)
+plt.title('Daily Closing Prices of QQQ from ' + start_date)
 plt.xlabel('Date')
-plt.ylabel('Closing Price (USD)')
-plt.legend()
-plt.grid(True)
+plt.ylabel('Closing Price ($)')
+
+# Save the plot to a file with the current date as the filename
+filename = datetime.now().strftime('%Y-%m-%d') + '_QQQ_Closing_Prices.png'
+plt.savefig(filename)
+
+# Display the plot (optional, you can comment this out if you don't want it to pop up)
 plt.show()
+
+print(f"File saved as {filename}")
